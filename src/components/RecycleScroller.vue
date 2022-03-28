a050523099323fefeaedd07cf22208fef00178e2
Christoffer <cborje@gmail.com>
2022-03-28 12:03:15 +0200

    testar

--- src/components/RecycleScroller.vue
+++ src/components/RecycleScroller.vue
@@ -29,6 +29,7 @@
         :style="ready ? { transform: `translate${direction === 'vertical' ? 'Y' : 'X'}(${view.position}px)` } : null"
         class="vue-recycle-scroller__item-view"
         :class="{ hover: hoverKey === view.nr.key }"
+        title="testar"
         v-on="detectHover ? { mouseenter: () => hoverKey = view.nr.key, mouseleave: () => hoverKey = null } : {}"
       >
         <slot
